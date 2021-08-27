cff9baa97ca9fb04cc24aa2ce90b221d05dacd10
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-08-27 13:10:23 +1000

    Add an override to the onTouchEvent() callback.

--- app/src/main/java/dev/peterhinch/assessmenttask2/WelcomeActivity.java
+++ app/src/main/java/dev/peterhinch/assessmenttask2/WelcomeActivity.java
@@ -1,14 +1,46 @@
 package dev.peterhinch.assessmenttask2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private final String DEBUG_TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    // Capture touch events for an Activity or View
+    // reference: https://developer.android.com/training/gestures/detector
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                Log.d(DEBUG_TAG, "Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d(DEBUG_TAG, "Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d(DEBUG_TAG, "Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d(DEBUG_TAG, "Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d(DEBUG_TAG, "Action was outside bounds of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
     }
 }
