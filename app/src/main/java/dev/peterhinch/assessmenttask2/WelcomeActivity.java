144c99184cc31efa35c7ac3e77fb11f987bef580
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-08-29 21:07:10 +1000

    Add animation resources for slide left and right. Implement animation for swipe right from welcome screen.

--- app/src/main/java/dev/peterhinch/assessmenttask2/WelcomeActivity.java
+++ app/src/main/java/dev/peterhinch/assessmenttask2/WelcomeActivity.java
@@ -1,46 +1,68 @@
 package dev.peterhinch.assessmenttask2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MotionEventCompat;
+import androidx.core.view.GestureDetectorCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+
+// Method for switching between activities with an animation from the GeeksForGeeks tutorial:
+// 'How to add slide animation between activities in android?'
+// reference: https://www.geeksforgeeks.org/how-to-add-slide-animation-between-activities-in-android/
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private final String DEBUG_TAG = this.getClass().getSimpleName();
+    private final String TAG = this.getClass().getSimpleName();
+
+    private final DisplayMetrics displayMetrics = new DisplayMetrics();
+
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        mDetector = new GestureDetectorCompat(this, new SliderGestureListener());
     }
 
-    // Capture touch events for an Activity or View
-    // reference: https://developer.android.com/training/gestures/detector
+    // This method opens the ListActivity screen with the specified animation from the resources
+    // directory.
+    public void openListActivity() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int action = MotionEventCompat.getActionMasked(event);
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
-        switch(action) {
-            case (MotionEvent.ACTION_DOWN) :
-                Log.d(DEBUG_TAG, "Action was DOWN");
-                return true;
-            case (MotionEvent.ACTION_MOVE) :
-                Log.d(DEBUG_TAG, "Action was MOVE");
-                return true;
-            case (MotionEvent.ACTION_UP) :
-                Log.d(DEBUG_TAG, "Action was UP");
-                return true;
-            case (MotionEvent.ACTION_CANCEL) :
-                Log.d(DEBUG_TAG, "Action was CANCEL");
-                return true;
-            case (MotionEvent.ACTION_OUTSIDE) :
-                Log.d(DEBUG_TAG, "Action was outside bounds of current screen element");
-                return true;
-            default :
-                return super.onTouchEvent(event);
+    class SliderGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+
+        // Detect a fling gesture and animate if the direction is as intended.
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                float velocityX, float velocityY) {
+            Log.d(TAG, "onFling: " + velocityX + "/" + velocityY);
+            // velocityX will be a value less than zero if the swipe is to the right.
+            if (velocityX < 0) {
+                openListActivity();
+            }
+            return true;
         }
     }
 }
