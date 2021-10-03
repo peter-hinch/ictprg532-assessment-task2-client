692dd3fc84aec7cea47577890c6ad71d91c04afc
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-03 18:21:25 +1100

    Add recycler view adapter for view_item.xml .

---
+++ app/src/main/java/dev/peterhinch/assessmenttask2/activities/ListRecyclerViewAdapter.java
@@ -0,0 +1,67 @@
+package dev.peterhinch.assessmenttask2.activities;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import dev.peterhinch.assessmenttask2.R;
+import dev.peterhinch.assessmenttask2.models.Record;
+
+// The recycler view adapter must extend RecyclerView.Adapter .
+public class ListRecyclerViewAdapter extends RecyclerView.Adapter<ListRecyclerViewAdapter.ListItemViewHolder> {
+    // Declare a dataset.
+    private ArrayList<Record> recordList;
+
+    // Create a constructor for the adapter class and dataset property.
+    public ListRecyclerViewAdapter(ArrayList<Record> recordList) {
+        this.recordList = recordList;
+    }
+
+    // Implement onCreateViewHolder, onBindViewHolder and getItemCount.
+    // Implement onCreateViewHolder
+    @NonNull
+    @Override
+    public ListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_item, parent, false);
+        return new ListItemViewHolder(v);
+    }
+
+    // Implement onBindViewHolder
+    @Override
+    public void onBindViewHolder(@NonNull ListItemViewHolder holder, int position) {
+        holder.txtViewHeading.setText(recordList.get(position).getHeading());
+        holder.txtViewDescription.setText(recordList.get(position).getDescription());
+        holder.txtViewPhone.setText(recordList.get(position).getPhone());
+        holder.txtViewDate.setText(recordList.get(position).getDate().toString());
+    }
+
+    // Implement getItemCount
+    @Override
+    public int getItemCount() {
+        return recordList == null ? 0 : recordList.size();
+    }
+
+    // Create an inner class that extends RecyclerView.ViewHolder .
+    static class ListItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtViewHeading;
+        private TextView txtViewDescription;
+        private TextView txtViewPhone;
+        private TextView txtViewDate;
+        // The inner class must have a constructor for the view holder
+        public ListItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Set the values in the item view
+            txtViewHeading = itemView.findViewById(R.id.itemView_textView_heading);
+            txtViewDescription = itemView.findViewById(R.id.itemView_textView_description);
+            txtViewPhone = itemView.findViewById(R.id.itemView_textView_phone);
+            txtViewDate = itemView.findViewById(R.id.itemView_textView_date);
+        }
+    }
+}
