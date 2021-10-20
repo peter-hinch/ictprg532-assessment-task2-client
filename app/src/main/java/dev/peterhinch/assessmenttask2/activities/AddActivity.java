1a8527d7281d7282da897d5330269125f05f48c6
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-20 14:22:11 +1100

    Add button listener for add button.

--- app/src/main/java/dev/peterhinch/assessmenttask2/activities/AddActivity.java
+++ app/src/main/java/dev/peterhinch/assessmenttask2/activities/AddActivity.java
@@ -3,14 +3,27 @@ package dev.peterhinch.assessmenttask2.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import dev.peterhinch.assessmenttask2.R;
 
 public class AddActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        // Set the click function for the Add button
+        addClick();
+    }
+
+    private void addClick(){
+        Button btnAdd = findViewById(R.id.add_button_add);
+        btnAdd.setOnClickListener(view -> {
+            Log.d(TAG, "Add button pressed.");
+        });
     }
 }
