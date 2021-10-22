94dc27576d6a0d4962ca96fa93350e93436dd2a0
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-22 13:52:19 +1100

    Add basic back functionality to button.

--- app/src/main/java/dev/peterhinch/assessmenttask2/activities/DetailActivity.java
+++ app/src/main/java/dev/peterhinch/assessmenttask2/activities/DetailActivity.java
@@ -2,15 +2,34 @@ package dev.peterhinch.assessmenttask2.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import dev.peterhinch.assessmenttask2.R;
 
 public class DetailActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // Set the click function for the 'Back' button.
+        backClick();
+    }
+
+    // Process the 'Back' button event.
+    private void backClick() {
+        Button btnBack = findViewById(R.id.detail_button_back);
+        btnBack.setOnClickListener(view -> {
+            Log.d(TAG, "Back button pressed.");
+
+            // Return to the list activity.
+            Intent intent = new Intent(DetailActivity.this, ListActivity.class);
+            startActivity(intent);
+        });
     }
 }
