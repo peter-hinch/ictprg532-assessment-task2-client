94c7301b73375c3dfbe2cdd8bc1b15ed0360b7f7
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-15 17:24:56 +1100

    Move functionality of 'ApiInterface' back to 'MyHashViewModel'.

--- app/src/main/java/dev/peterhinch/assessmenttask2/viewmodels/MyHashViewModel.java
+++ app/src/main/java/dev/peterhinch/assessmenttask2/viewmodels/MyHashViewModel.java
@@ -3,15 +3,20 @@ package dev.peterhinch.assessmenttask2.viewmodels;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import dev.peterhinch.assessmenttask2.lib.MyHash;
+import dev.peterhinch.assessmenttask2.retrofit.RetrofitServices;
 import dev.peterhinch.assessmenttask2.room.LocalRecordDb;
 import dev.peterhinch.assessmenttask2.room.entities.Record;
 
-public class MyHashViewModel extends ViewModel {
+public class MyHashViewModel extends ViewModel
+        implements RetrofitServices.ResultsHandler {
 
     private final String TAG = this.getClass().getSimpleName();
 
@@ -56,9 +61,54 @@ public class MyHashViewModel extends ViewModel {
         // Retrieve data from the database and hash that data.
         try {
             recyclerViewRecords = (ArrayList<Record>) LocalRecordDb.recordReadAll(context);
+            try {
+                RetrofitServices.getInstance().recordReadAll(this);
+            } catch (Exception ex) {
+                Log.d(TAG, "Problem when attempting to read records from remote " +
+                        "database: " + ex);
+            }
             reHashRecords();
          } catch (Exception ex) {
             Log.d(TAG, "Exception occurred while reading in records: " + ex);
+        }
+    }
+
+    @Override
+    public void createOnResponseHandler(Record returnedRecord) {
+        Log.d(TAG, returnedRecord + " received by ListActivity CreateOnResponseHandler.");
+    }
+
+    @Override
+    public void readOneOnResponseHandler(Record returnedRecord) {
+        Log.d(TAG, returnedRecord + " received by ListActivity ReadOneOnResponseHandler.");
+    }
+
+    @Override
+    public void readAllOnResponseHandler(@NonNull List<Record> returnedRecordList) {
+        Log.d(TAG, returnedRecordList.size() + " records received by ListActivity ReadAllOnResponseHandler.");
+        recyclerViewRecords.clear();
+        for (Record r : returnedRecordList) {
+            recyclerViewRecords.add(r);
+            LocalRecordDb.recordCreate(null, r);
+        }
+    }
+
+    @Override
+    public void updateOnResponseHandler() {
+        Log.d(TAG, "Update received by ListActivity UpdateOnResponseHandler.");
+    }
+
+    @Override
+    public void deleteOnResponseHandler(Record returnedRecord) {
+        Log.d(TAG, returnedRecord + " received by ListActivity DeleteOnResponseHandler.");
+    }
+
+    @Override
+    public void onFailureHandler(Throwable throwable) {
+        if (throwable instanceof IOException) {
+            Log.d(TAG, "Failure received by OnFailureHandler: There is an issue with the connection.");
+        } else {
+            Log.d(TAG, "Failure received by OnFailureHandler: " + throwable.toString());
         }
     }
 }
